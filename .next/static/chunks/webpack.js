1e61bcc0d772caf2089d62d474b668ce2ebf73c0
marayissi <mara.ayissi@hotmail.com>
2022-11-10 11:31:25 +0100

    started baby registration

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bf983f75b6f6ff63"; }
+/******/ 		__webpack_require__.h = function() { return "f0e049a4712320d4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ pages/home.js
@@ -0,0 +1,3 @@
+export default function Home2(){
+  return <h1>hi</h1>
+}
